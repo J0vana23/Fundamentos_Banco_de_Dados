d5f83d83422d153916ea06341cc6720bd85fe494
Jovana <125403554+J0vana23@users.noreply.github.com>
2024-06-06 14:47:17 -0300

    Update pessoaController.php

--- aula_25_04/controller/pessoaController.php
+++ aula_25_04/controller/pessoaController.php
@@ -1,16 +1,19 @@
 <?php
-
+// INCLUI O ARQUIVO CONTENDO A DEFINIÇÃO DA CLASSE PESSOA//
 require_once '../model/pessoa.php';
 
+// CRIAÇÃO DE UMA CLASSE CONTROLLER//
 class pessoaController{
+    // DECLARAÇÃO DE UMA PROPRIEDADE PRIVADA $PESSOA, QUE SERÁ UM OBJETO DA CLASSE PESSOA//
     private $pessoa;
    
     public function __construct(){
-        $this->pessoa = new Pessoa();
-        $this->inserir();
+        $this->pessoa = new Pessoa(); // INSTANCIA UM NOVO OBJETO DA CLASSE PESSOA E ATRIBUI À PROPRIEDADE $PESSOA//
+        $this->inserir();  // CHAMA O MÉTODO INSERIR() ASSIM QUE UM OBJETO DA CLASSE PESSOACONTROLLER É CRIADO//
     }
 
     public function inserir(){
+        // DEFINE OS VALORES DAS PROPRIEDADES DA PESSOA COM BASE NOS DADOS RECEBIDOS VIA POST
         $this-> pessoa->setNome($_POST['nome']);
         $this-> pessoa->setEndereco($_POST['endereco']);
         $this-> pessoa->setBairro($_POST['bairro']);
@@ -19,9 +22,11 @@ class pessoaController{
         $this-> pessoa->setEstado($_POST['estado']);
         $this-> pessoa->setTelefone($_POST['telefone']);
         $this-> pessoa->setCelular($_POST['celular']);     
-        
+
+        // CHAMA O MÉTODO INSERIR() DA CLASSE PESSOA PARA INSERIR OS DADOS DA PESSOA NO BANCO DE DADOS//
         $this->pessoa->inserir();
     }
 }
+// CRIA UMA NOVA INSTÂNCIA DA CLASSE PESSOACONTROLLER, O QUE ACIONARÁ A EXECUÇÃO DO CÓDIGO ACIMA//
 new pessoaController
 ?>
