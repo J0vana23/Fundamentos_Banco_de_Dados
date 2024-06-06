e4e6bb2dd7fa944519e4514936dbb5499d888153
Jovana <125403554+J0vana23@users.noreply.github.com>
2024-06-06 14:59:57 -0300

    Alteração de comentários

--- aula_25_04/controller/conexao.php
+++ aula_25_04/controller/conexao.php
@@ -1,22 +1,26 @@
 <?php
 
-//CLASSE CONEXÃO CONECTADA COM BANCO DE DADOS DO LOCALHOST//
+// CLASSE CONEXÃO CONECTADA COM BANCO DE DADOS DO LOCALHOST//
 class Conexao{
-    private $host = "localhost";
-    private $usuario = "root";
-    private $senha = "";
+    private $host = "localhost";  // ENDEREÇO DO SERVIDOR MYSQL (LOCALHOST)//
+    private $usuario = "root";   // NOME DE USUÁRIO DO MYSQL//
+    private $senha = ""; // SENHA DO MYSQL (POR ENQUANTO ESTA VAZIA)//
     private $banco = "exemplo_aula_25_04"; //NOME DO BANCO DE DADOS//
-    private $conexao;
+    private $conexao;  // VARIÁVEL PARA ARMAZENAR A CONEXÃO MYSQL//
 
-    //QUANDO A CLASSE FOR LIDA VAI SER EXECUTADA O QUE ESTÁ AQUI DENTRO//
+    // QUANDO A CLASSE FOR LIDA VAI SER EXECUTADA O QUE ESTÁ AQUI DENTRO//
     public function __construct() {
+      
+        // CRIA UMA NOVA CONEXÃO MYSQL USANDO OS DADOS FORNECIDOS//
        $this->conexao = new mysqli($this->host, $this->usuario, $this->senha, $this->banco);
 
+       // VERIFICAR SE HOUVER FALHA NA CONEXÃO//
        if ($this->conexao->connect_error) {
+        // MENSAGEM QUE APARECERÁ SE HOUVER FALHA//
         die("Falha na conexão: " . $this->conexao->connect_error); }
-
     }
-    
+  
+    // MÉTODO PARA OBTER A CONEXÃO COM O BANCO DE DADOS
     public function getConexao() {
         return $this->conexao;
     }
